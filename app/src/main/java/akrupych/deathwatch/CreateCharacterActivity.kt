fe28d61a61f47651e89fba54eb9f078838042368
Andriy Krupych <andriy.krupych@navico.com>
2019-09-12 03:54:46 +0300

    Initial commit

---
+++ app/src/main/java/akrupych/deathwatch/CreateCharacterActivity.kt
@@ -0,0 +1,12 @@
+package akrupych.deathwatch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateCharacterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_character_activity)
+    }
+}
